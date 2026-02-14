0ecda16c2fb529910715f824979c63adfa505998
RomyCardozo <romycardozopy@gmail.com>
2026-02-14 17:18:31 -0300

    Inicializacion de la estructura del proyecto

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/api/v1/health-check", (req, res) => {
+    res.json({
+        status: "Online",
+        health: "ok"
+})
+})
+
+app.get("/api/v1/saludar", (req, res) => {
+    res.send("Holi");
+})
+
+app.listen(PORT, () => {
+    console.log("Bank Api corriendo en el puerto:", PORT)
+})
